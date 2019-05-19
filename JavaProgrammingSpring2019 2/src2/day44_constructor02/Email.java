12c865020e8befbb09b159f6774272a58e3487e1
ExHar0n <exhar0n@W7E1418250.localdomain>
2019-05-19 12:10:17 -0400

    bug fixes

--- JavaProgrammingSpring2019 2/src2/day44_constructor02/Email.java
+++
@@ -1,9 +0,0 @@
-package day44_constructor02;
-
-public class Email {
-	String message;
-	public Email(String message) {
-		System.out.println("Sending email with message: " + message);
-		this.message = message;
-	}
-}
